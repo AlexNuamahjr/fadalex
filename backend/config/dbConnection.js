5053a6829cf0a3dd1c50e5ced68b689214782d28
AlexNuamah <alexandernuamahjr@gmail.com>
2023-12-10 19:03:42 +0000

    Update: ES6 format

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -3,11 +3,11 @@ import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-DB_NAME = process.env.DB_NAME;
-DB_USER = process.env.DB_USER;
-DB_PASS = process.env.DB_PASS;
-DB_HOST = process.env.DB_HOST;
-DB_DIALECT = process.env.DB_DIALECT;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_DIALECT = process.env.DB_DIALECT;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS,{
     host: DB_HOST,
